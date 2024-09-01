492923303c35ba2b60592ba42d2c05694f211d9e
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2024-09-01 16:04:17 +0500

    Add files via upload

---
+++ firstPython.py
@@ -0,0 +1,44 @@
+# These are simple outputs with print() built in function
+"""
+print("Hello World")
+print("Umair Sajawal")
+print("Hello world,", "Its me Umair Sajawal")
+print(32)
+print(10+2)
+print(10-2)
+print(10*2)
+print(10/2)
+print(10%2)
+"""
+# This is variables in python. syntax: var_name = value
+"""
+name = "Umair Sajawal"
+age = 21
+price = 25.99
+print(name)
+print(name, age, price)
+age2 = age
+print(age2)
+print("My name is :", name)
+print("My age is :", age)
+
+# type(): This is used now the type of the variable
+print(type(name))
+print(type(age))
+print(type(price))
+"""
+# Datatypes in python:
+
+'''
+1. Integer (+ve, -ve, 0)
+2. String  ('hello' or "hello" or ''hello'')
+3. Float   (for example: 25.5, 65.29, ...)
+4. Boolean (only two conditions 1.True or 2.False. Note in boolean syntax T and F always write capital)
+5. None    (for example: a = None, means want not to store any value in variable a)
+'''
+age = 25
+old = False
+a = None
+print(type(age))
+print(type(old))
+print(type(a))
