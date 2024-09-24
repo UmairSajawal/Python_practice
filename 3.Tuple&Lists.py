eff384f86c0007cb35165e5b381b6cf59b435cbe
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2024-09-24 22:40:35 +0500

    Update 3.Tuple&Lists.py

--- 3.Tuple&Lists.py
+++ 3.Tuple&Lists.py
@@ -34,7 +34,7 @@ print(marks[-3:-1])   #Output : [60, 56]
 """
 
 # List Methods or Functions
-
+"""
 list = [2, 1, 3]
 '''
 list.append(4)  #adds one element at the end  [2, 1, 3, 4]
@@ -55,3 +55,57 @@ list2.remove(1)  #removes first occurrence of element
 list2.pop(2)
 
 '''read more function from: python documentation'''
+"""
+
+# Tuples in python
+# Note: Tuples are immutable while list are mutable
+""""
+'''Use () brackets in tuples'''
+tup = (1,2,3,5)
+tup1 = ()
+print(tup1)  #Output: ()
+tup2 = (1,)  
+print(tup2)  #Output: (1,)
+tup3 = (1)  
+
+print(tup3)  #Output: 1. Python understand that this is an integer if you not separate through comma. 
+#if write (1.0) understand as float and if write ("hello") understand as string
+
+print(type(tup))
+print(tup[0])
+print(tup[1])
+''' tup[0] = 5 # Not allowed because tuples are immutable (not change) '''
+# Tuple slicing method same as the method of list slicing
+"""
+
+# Tuple Methods
+"""""
+tup0 = (2, 1, 3, 1)
+tup0.index(3) #Returns the index of the first occurrence. tup0.index(1) is 1. This is use to find index number.
+print(tup0.index(3))  #Output: 2
+
+tup0.count(1) #Count total occurrence. how many time exists in tuple. tup0.count(1) is 2 times.
+"""
+
+# Pracite Set
+'''
+movies = []
+movie1 = input("Enter first favorite movie name ;")
+movie2 = input("Enter second favorite movie name ;")
+movie3 = input("Enter third favorite movie name ;")
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)  # Output:  ['carry on jatta', 'load wedding', 'chal mera put']
+'''
+list1 = [1,2,3,2,1]
+list_copy = list1.copy()
+list_copy.reverse()
+if (list_copy == list1):
+    print("Palindrome")
+else:
+    print("No palindrome")
+    
+grade = ["b", "c", "d", "a", "e"]
+grade.sort()
+print(grade) #Output: ['a', 'b', 'c', 'd', 'e']
