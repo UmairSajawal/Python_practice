13fbd8b3653796ee5b9bcb5e72136e966a78a576
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2024-10-07 23:02:31 +0500

    Update 4.dictionary&sets.py

--- 4.dictionary&sets.py
+++ 4.dictionary&sets.py
@@ -100,15 +100,86 @@ print(students("name")) #Output: Haider
 print(students("name2")) #Output: Error
 '''
 # 5. Update( newDict ) #inserts the specified items to the dictionary
-
+"""
 students.update({"gender" : "male"})
 print(students) #Output: {'name': 'Haider', 'subjects': {'phy': 70, 'chem': 60, 'math': 50}, 'age': 18, 'city': 'lahore', 'gender': 'male'}
 
 new_dict = {"name" : "ali", "Province" : "Punjab"} #Duplicate "keys" are not allowed in Dictionaries
 students.update(new_dict)
 print(students)  #Output: {'name': 'ali', 'subjects': {'phy': 70, 'chem': 60, 'math': 50}, 'age': 18, 'city': 'lahore', 'gender': 'male', 'Province': 'Punjab'}
+"""
 
+# Sets
+'''
+-Set is the collection of the unordered items.
+-Each element in the set must be unique & immutable.
+-lists and dictionaries are not stored in Sets because Set elements are immutable (Not Change) while lists and dictionaries are mutable changeable)
+-Duplicate values are ignored
+-Values in random order or way
+'''
+"""
+set2 = {1, 2, 3, "Hello", "World", "Hello"}
+print(set2)  #Output : {1, 2, 3, 'World', 'Hello'}
+print(type(set2))
 
+set3 = {} #This not empty Set. This is empty Dictionary
+set4 = set() #This is empty Set syntax
+print(type(set4))
+"""
 
+# Set Methods:
+'''
+-set.add( el ) #adds an element
+-set.remove( el ) #removes the element
+-set.clear( ) #empties the set
+-set.pop( ) #removes a random value
+-set.union( set2 ) #combines both set values & returns new and same value count only one time in output
+-set.intersection( set2 ) #combines common values & returns new
 
+Sets -> Mutable
+Sets -> Elements -> Immutable
+'''
+"""
+collSet = {6, 7, 8} 
+collSet.add(9)  #adds an element
+collSet.add(13)
+collSet.add("Umair") #string
+collSet.add((1,2,3)) #Tuple
+print(collSet)     #output : {6, 7, 8, 9, (1, 2, 3), 13, 'Umair'}
+collSet.remove(13) #removes the element
+print(collSet)     #Output : {6, 7, 8, 9, (1, 2, 3), 'Umair'}
 
+collSet.clear() #empties the set
+print(len(collSet)) #Output : 0
+
+collection = {"helo", "word", "umair", "sajawal"}
+collection.pop()   #removes a random value
+print(collection)  #Output : {'word', 'sajawal', 'umair'}
+
+set6 = {1,2,3}
+set7 = {3,4,5}
+print(set6.union(set7))        #Union         #Output : {1, 2, 3, 4, 5}
+print(set6.intersection(set7)) #Intersection  #Output : {3}
+"""
+
+# Practice Set:
+'''
+dictionary = {
+    "cat" : "a small animal",
+    "table" : ["a piece of furniture", "list of facts & figures"]
+}
+print(dictionary)
+
+sets = {"python","java", "C++", "python", "javascript", "java", "python", 'java', "C++", "C"}
+print(len(sets)) 
+'''
+marks = {}
+x = int(input("enter your phy marks:"))
+marks.update({"phy" : x})
+
+x = int(input("enter your maths marks:"))
+marks.update({"maths" : x})
+
+x = int(input("enter your chem marks:"))
+marks.update({"chem" : x})
+print(marks)
