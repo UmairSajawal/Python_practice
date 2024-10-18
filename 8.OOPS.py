4be00e630719b8416b76a6280cf2e89c19e5fd5f
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2024-10-18 22:47:16 +0500

    Update 8.OOPS.py

--- 8.OOPS.py
+++ 8.OOPS.py
@@ -53,11 +53,31 @@ self.name =  fullname
 s1 =  Student( “khuram” )
 print(s1.name) 
 '''
+"""
 class Student:
     name = "zubair"
-    def __init__(self):    #(self) basically point to Student()
+    def __init__(self):    #Default Constructor  #(self) basically point to Student()
         print("adding new student in the databse...")
     
 s1 = Student() #Output when run this code: adding new student in the databse...
 #counstructor invoke automatically
 
+class Student:
+    def __init__(self, fullName):   #Parameterized Constructor()
+        self.name = fullName
+        print("adding new student in database...")
+s1 = Student("Umair Sajawal")
+print(s1.name)    #Output : Umair Sajawal
+
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("adding new record in database")
+s1 = Student("jameel ahmed", 80)
+print(s1.name, s1.marks)
+
+s2 = Student("Shahaan jutt", 90)
+print(s2.name, s2.marks)
+"""
